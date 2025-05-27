08a62d62f7008ec0394d33c69105b334872c111d
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-27 21:15:49 +0530

    Common js module

--- Basic CommonJS/file-1.js
+++ Basic CommonJS/file-1.js
@@ -1,4 +1,5 @@
 // const varriable = require("./file-2");
 const { a, add } = require("./file-2");
-console.log(a);
-console.log(add(10, 4));
+const { a: a3, add: add3 } = require("./file-3");
+console.log(a3);
+console.log(add3(10, 4, 5));
---
+++ Basic CommonJS/file-3.js
@@ -0,0 +1,12 @@
+const a = 10;
+
+const add = (num1, num2, num3) => {
+  return num1 + num2 + num3;
+};
+module.exports = {
+  a,
+  add,
+};
+// module.exports = add;
+// module.exports = a;
+// console.log(module);
