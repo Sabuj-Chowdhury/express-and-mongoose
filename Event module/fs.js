81b2554b6d505432f51fe493d634c829acd54c96
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-29 20:07:48 +0530

    write in file

--- Event module/fs.js
+++ Event module/fs.js
@@ -1,5 +1,12 @@
 // fs->file system
 const fs = require("fs");
 
+const myText = "Writing in file";
+
+// writing in file
+fs.writeFileSync("./hello.txt", myText);
+
+// reading in file
 const data = fs.readFileSync("./hello.txt", { encoding: "utf-8" });
+
 console.log(data);
