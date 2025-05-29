5e94591f2bd1694249e9796eacc9075dcf315758
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-29 21:01:05 +0530

    async writing

--- Event module/fsAsync.js
+++ Event module/fsAsync.js
@@ -11,6 +11,17 @@ fs.readFile("./hello.txt", { encoding: "utf-8" }, (err, data) => {
   console.log(data);
 });
 console.log(2);
+// writing data
+const txt = "ki koro?";
+
+fs.writeFile("./hello.txt", txt, { encoding: "utf-8" }, (err) => {
+  if (err) {
+    console.log("err er jonno write hoi nai", err);
+    return;
+  }
+  console.log("write operation done");
+});
+console.log(5);
 
 fs.readFile("./hello.txt", { encoding: "utf-8" }, (err, data) => {
   if (err) {
