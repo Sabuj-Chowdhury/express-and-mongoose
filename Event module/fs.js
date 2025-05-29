89a14cb95990741f94dde061fdfecc77d2174e86
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-05-29 20:02:24 +0530

    why encoding

--- Event module/fs.js
+++ Event module/fs.js
@@ -1,5 +1,5 @@
 // fs->file system
 const fs = require("fs");
 
-const data = fs.readFileSync("./hello.txt");
+const data = fs.readFileSync("./hello.txt", { encoding: "utf-8" });
 console.log(data);
