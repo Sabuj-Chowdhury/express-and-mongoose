773220ca6946f1c3b36896396d35120902aec3ce
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-01 19:06:14 +0530

    Very basic http routing in node

--- todo-app/server.js
+++ todo-app/server.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log({ req, res });
-  res.end("welcome ........");
+  //   console.log(req.url, req.method);
+  //   res.end("welcome ........");
+  if (req.url === "/todos" && req.method === "GET") {
+    res.end("All todos");
+  } else if ((req.url = "/todos/create-todo" && req.method === "POST")) {
+    res.end("Todo created");
+  } else {
+    res.end("Route not found!");
+  }
 });
 
 server.listen(5000, "127.0.0.1", () => {
