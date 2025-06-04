286bdceee7e72be2fa76f0cfb84c1fb83e02188a
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-04 11:00:21 +0530

    Organizing Codes & Splitting The Routes

--- ToDo Express/src/app/app.ts
+++ ToDo Express/src/app/app.ts
@@ -1,17 +1,12 @@
 import express, { Application, Request, Response } from "express";
+import { todosRouter } from "./todos/todos.route";
 const app: Application = express();
 
 app.use(express.json());
+app.use("/todos", todosRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to ToDo's app");
-});
-
-app.get("/todos", (req: Request, res: Response) => {
-  res.send("Hello world!");
-});
-app.post("/todos/create-todo", (req: Request, res: Response) => {
-  res.send("Hello world!");
 });
 
 export default app;
---
+++ ToDo Express/src/app/todos/todos.route.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+
+// router middleware
+export const todosRouter = express.Router();
+
+todosRouter.get("/", (req: Request, res: Response) => {
+  res.send("All todos");
+});
+
+todosRouter.post("/create-todo", (req: Request, res: Response) => {
+  res.send("Create todos");
+});
+
+todosRouter.get("/:title", (req: Request, res: Response) => {
+  res.send("get todos by title ");
+});
+todosRouter.put("/update-todo/:title", (req: Request, res: Response) => {
+  res.send("Update todo");
+});
+todosRouter.delete("/delete-todo/:title", (req: Request, res: Response) => {
+  res.send("Delete todo");
+});
