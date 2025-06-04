c955f65b55bb534c0f981779a6e1eac6aa683903
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-04 12:23:56 +0530

    Get Single ToDO, Updating & Deleting ToDos

--- ToDo Express/src/app/todos/todos.route.ts
+++ ToDo Express/src/app/todos/todos.route.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import { client } from "../../config/mongoDB";
+import { ObjectId } from "mongodb";
 
 // router middleware
 export const todosRouter = express.Router();
@@ -29,12 +30,32 @@ todosRouter.post("/create-todo", async (req: Request, res: Response) => {
   res.send(todos);
 });
 
-todosRouter.get("/:title", (req: Request, res: Response) => {
-  res.send("get todos by title ");
+todosRouter.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const db = await client.db("practiceTodoBD");
+  const collection = await db.collection("todos");
+  const todo = await collection.findOne({ _id: new ObjectId(id) });
+  res.send(todo);
 });
-todosRouter.put("/update-todo/:title", (req: Request, res: Response) => {
-  res.send("Update todo");
+
+todosRouter.put("/update-todo/:id", async (req: Request, res: Response) => {
+  const db = await client.db("practiceTodoBD");
+  const collection = await db.collection("todos");
+  const id = req.params.id;
+  const { title, description, priority, isCompleted } = req.body;
+
+  const filter = { _id: new ObjectId(id) };
+  const UpdateDoc = await collection.updateOne(
+    filter,
+    { $set: { title, description, priority, isCompleted } },
+    { upsert: true }
+  );
+  res.send(UpdateDoc);
 });
-todosRouter.delete("/delete-todo/:title", (req: Request, res: Response) => {
-  res.send("Delete todo");
+todosRouter.delete("/delete-todo/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const db = await client.db("practiceTodoBD");
+  const collection = await db.collection("todos");
+  const response = await collection.deleteOne({ _id: new ObjectId(id) });
+  res.send(response);
 });
