f57a9c70504bbdf5133ffcb8726470007eac1692
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-04 12:04:29 +0530

    POST and GET todos

--- ToDo Express/src/app/server.ts
+++ ToDo Express/src/app/server.ts
@@ -1,28 +1,12 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { client } from "../config/mongoDB";
 import app from "./app";
 const port = 5000;
 
 let server;
 
-const uri =
-  "mongodb+srv://test_user:sxKBfy8pKTmiGdjB@cluster0.i53p4.mongodb.net/practiceTodoBD?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
 const bootstrap = async () => {
   await client.connect();
   console.log("Connected to mongoDB");
-
-  const db = await client.db("practiceTodoBD");
-  const collection = await db
-    .collection("todos")
-    .insertOne({ title: "mongoDb", body: "mongodb" });
 
   server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- ToDo Express/src/app/todos/todos.route.ts
+++ ToDo Express/src/app/todos/todos.route.ts
@@ -1,15 +1,32 @@
 import express, { Request, Response } from "express";
+import { client } from "../../config/mongoDB";
 
 // router middleware
 export const todosRouter = express.Router();
 
-todosRouter.get("/", (req: Request, res: Response) => {
-  res.send("All todos");
+todosRouter.get("/", async (req: Request, res: Response) => {
+  const db = await client.db("practiceTodoBD");
+  const collection = await db.collection("todos");
+
+  const todos = await collection.find().toArray();
+  res.send(todos);
 });
 
-todosRouter.post("/create-todo", (req: Request, res: Response) => {
+todosRouter.post("/create-todo", async (req: Request, res: Response) => {
   // title //description //priority : high/medium/low //isCompleted : true/false
-  res.send("Create todos");
+
+  const { title, description, priority } = req.body;
+
+  const db = await client.db("practiceTodoBD");
+  const collection = await db.collection("todos");
+  collection.insertOne({
+    title: title,
+    description: description,
+    priority: priority,
+    isCompleted: false,
+  });
+  const todos = await collection.find().toArray();
+  res.send(todos);
 });
 
 todosRouter.get("/:title", (req: Request, res: Response) => {
