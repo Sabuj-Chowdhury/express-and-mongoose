2ef6fb8244ba02739f6c587e4fa570cd2a47c5c2
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-04 12:29:26 +0530

    Custom Middleware in Express

--- ToDo Express/src/app/app.ts
+++ ToDo Express/src/app/app.ts
@@ -1,12 +1,25 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { todosRouter } from "./todos/todos.route";
 const app: Application = express();
 
 app.use(express.json());
 app.use("/todos", todosRouter);
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to ToDo's app");
-});
+app.get(
+  "/",
+  (req: Request, res: Response, next: NextFunction) => {
+    console.log("Custom middleware");
+    console.log({
+      url: req.url,
+      method: req.method,
+      header: req.header,
+    });
+    next();
+  },
+
+  (req: Request, res: Response) => {
+    res.send("Welcome to ToDo's app");
+  }
+);
 
 export default app;
