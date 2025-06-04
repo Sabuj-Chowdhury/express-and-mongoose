fb60aba47043e019295647ebca46ed6e0caabdf1
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-04 12:59:12 +0530

    Error Handling in Express: 404s & Global Error Handler

--- ToDo Express/src/app/app.ts
+++ ToDo Express/src/app/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 import { todosRouter } from "./todos/todos.route";
+import { error } from "console";
 const app: Application = express();
 
 app.use(express.json());
@@ -17,9 +18,36 @@ app.get(
     next();
   },
 
-  (req: Request, res: Response) => {
-    res.send("Welcome to ToDo's app");
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // console.log(what);
+      res.send("Welcome to ToDo's app");
+    } catch (error) {
+      next(error);
+    }
   }
 );
+
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    console.log(what);
+    res.send("Welcome to Error er duniya");
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log("Error", error);
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+});
 
 export default app;
