3bcea96d30962d1c13d2301b82271c23a15a5253
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-19 10:36:25 +0530

    MongoDB connection using mongoose

--- advanced-note-app-with-mongoose/src/app.ts
+++ advanced-note-app-with-mongoose/src/app.ts
@@ -1,5 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to note app");
+});
 
 export default app;
--- advanced-note-app-with-mongoose/src/server.ts
+++ advanced-note-app-with-mongoose/src/server.ts
@@ -1,5 +1,6 @@
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
 
 let server: Server;
 
@@ -7,6 +8,12 @@ const PORT = 5000;
 
 async function main() {
   try {
+    await mongoose.connect(
+      `mongodb+srv://noteApp:ltSjDR4gUW4mCQMK@cluster0.i53p4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+
+    console.log("Connected to Mongodb using mongoose!!!");
+
     server = app.listen(PORT, () => {
       console.log(`App is listening on: ${PORT}`);
     });
@@ -14,3 +21,5 @@ async function main() {
     console.log(err);
   }
 }
+
+main();
