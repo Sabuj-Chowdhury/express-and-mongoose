d3f301b7639aa2e2d4cf53c677cf972294f41891
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-19 20:58:00 +0530

    Validate using Zod

--- advanced-note-app-with-mongoose/src/app/controller/user.controller.ts
+++ advanced-note-app-with-mongoose/src/app/controller/user.controller.ts
@@ -1,18 +1,39 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/user.model";
+import { string, z } from "zod";
 
 export const userRouter = express.Router();
 
+const CreateUserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+});
+
 userRouter.post("/create-user", async (req: Request, res: Response) => {
-  const body = req.body;
+  try {
+    // const body = req.body;
+    const body = await CreateUserZodSchema.parseAsync(req.body);
+    console.log("zod body ", body);
 
-  const user = await User.create(body);
+    const user = await User.create(body);
 
-  res.status(201).json({
-    success: true,
-    message: "User created successfully!",
-    user,
-  });
+    res.status(201).json({
+      success: true,
+      message: "User created successfully!",
+      user,
+    });
+  } catch (err: any) {
+    console.log(err);
+    res.status(400).json({
+      success: false,
+      message: err.message,
+      err,
+    });
+  }
 });
 
 userRouter.get("/", async (req: Request, res: Response) => {
