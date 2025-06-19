d963b09961f136ce96ab4f7709b8fa9236b06b69
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-19 11:50:04 +0530

    Data Types and Required Fields in Schema

--- advanced-note-app-with-mongoose/src/app.ts
+++ advanced-note-app-with-mongoose/src/app.ts
@@ -4,8 +4,20 @@ import { model, Schema } from "mongoose";
 const app: Application = express();
 
 const noteSchema = new Schema({
-  title: String,
-  content: String,
+  //   title: String,
+  //   content: String,
+  title: { type: String, required: true, trim: true },
+  content: { type: String, default: "" },
+  category: {
+    type: String,
+    enum: ["personal", "study", "work", "other"],
+    default: "personal",
+  },
+  pinned: { type: Boolean, default: false },
+  tags: {
+    label: { type: String, required: true },
+    color: { type: String, default: "Green" },
+  },
 });
 
 const Note = model("Note", noteSchema);
@@ -13,7 +25,6 @@ const Note = model("Note", noteSchema);
 app.post("/create-note", async (req: Request, res: Response) => {
   const myNote = new Note({
     title: "Learning Mongoose",
-    content: "I am learning Mongoose",
   });
 
   await myNote.save();
