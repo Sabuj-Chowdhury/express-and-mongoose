0dfbeb1dc1424d8b072329e7640957adcd0de5ba
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-19 10:19:48 +0530

    basic http server and port listen

---
+++ advanced-note-app-with-mongoose/src/server.ts
@@ -0,0 +1,16 @@
+import { Server } from "http";
+import app from "./app";
+
+let server: Server;
+
+const PORT = 5000;
+
+async function main() {
+  try {
+    server = app.listen(PORT, () => {
+      console.log(`App is listening on: ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
