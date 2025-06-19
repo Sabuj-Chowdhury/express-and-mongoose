6c3094cbd56927feda5a07e75781363869209ef0
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-06-19 20:11:22 +0530

    Validations in Mongoose: Built-in and Custom validations [max/min]

--- advanced-note-app-with-mongoose/src/app/models/user.model.ts
+++ advanced-note-app-with-mongoose/src/app/models/user.model.ts
@@ -3,14 +3,41 @@ import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema<IUser>(
   {
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
-    email: { type: String, required: true, trim: true },
-    password: { type: String, required: true },
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [5, "first name must be at least 5 charecter, got {VALUE}"],
+      maxlength: 15,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      // min:18,
+      min: [18, "Age must be greater than 18 , got {VALUE}"],
+      max: 60,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
     role: {
       type: String,
-      enum: ["admin", "user"],
-      default: "user",
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+      uppercase: true,
     },
   },
   {
